2768b9b9667a52a52424b4ba5870de2027da718d
eaguilar112 <erwin.aguilar123@outlook.com>
2025-02-06 13:28:16 -0700

    everything back

--- functions.js
+++ functions.js
@@ -25,6 +25,59 @@ const getConversion = (from) => {
     };
 }
 
+document.getElementById("calc").addEventListener("click", (event) => {
+
+    event.preventDefault();
+
+    const kgToLbValue = (document.getElementById("kgToLb").value);
+    const lbToKgValue = (document.getElementById("lbToKg").value);
+
+    let result = [];
+
+    const isKgToLb = document.getElementById("default-radio-2").checked;
+
+    if (isKgToLb) {
+
+        const kgValues = kgToLbValue.split(',').map(val => parseFloat(val.trim()));
+        if (kgValues.every(val => !isNaN(val))) {
+            const kgToLbConverter = getConversion("kgToLb");
+            result = kgToLbConverter(kgValues);
+        } else {
+            result = ["Invalid input for KG"];
+        }
+    } else {
+        const lbValues = lbToKgValue.split(',').map(val => parseFloat(val.trim()));
+        if (lbValues.every(val => !isNaN(val))) {
+            const lbToKgConverter = getConversion("lbToKg");
+            result = lbToKgConverter(lbValues);
+        }
+    }
+
+
+    document.getElementById("result").value = result.join(', ');
+
+});
+
+document.getElementById("default-radio-1").addEventListener("click", (event) => {
+
+    if (event.target.checked) {
+        document.getElementById("kgToLb").readOnly = true;
+        document.getElementById("lbToKg").removeAttribute("readonly");
+        document.getElementById("kgToLb").value = "";
+        document.getElementById("result").value = "";
+    }
+
+});
+
+document.getElementById("default-radio-2").addEventListener("click", (event) => {
+    if (event.target.checked) {
+        document.getElementById("kgToLb").readOnly = false;
+        document.getElementById("lbToKg").readOnly = true;
+        document.getElementById("lbToKg").value = "";
+        document.getElementById("result").value = "";
+    }
+})
+
 function openTab(tabId) {
     let tabs = document.querySelectorAll('div[id^="tab"]');
     tabs.forEach(tab => tab.style.display = "none");
